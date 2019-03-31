e9331a5ab49158c65f0e43dd9ba5b16bfa1d216e
startsevdev <startsevdev@gmail.com>
2019-03-31 16:25:05 +0300

    added /help and /start commands. corrected /report command

--- bot.py
+++ bot.py
@@ -4,13 +4,15 @@ import telebot
 from telebot import types
 from db import Database
 from parser import Parser
+from csv_generator import CSVGenerator
 sys.path.append('../')
 import tokens
 
 
-bot = telebot.TeleBot(tokens.KKCashboxBot, threaded=False)
+bot = telebot.TeleBot(tokens.CoffeeCultureBot, threaded=False)
 db = Database("/Users/alexander/code/bots/CashboxBot/data.db")
 parser = Parser()
+csv_generator = CSVGenerator()
 
 
 def keyboard():
@@ -26,6 +28,12 @@ def console_print(message):
     print("{} | {}: {}".format(now, message.from_user.id, message.text))
 
 
+@bot.message_handler(commands=["start"])
+def start(message):
+    console_print(message)
+    bot.send_message(message.from_user.id, db.revenue(), reply_markup=keyboard())
+
+
 @bot.message_handler(commands=["revenue"])
 def revenue(message):
     console_print(message)
@@ -33,9 +41,31 @@ def revenue(message):
 
 
 @bot.message_handler(commands=["report"])
-def revenue(message):
+def report(message):
     console_print(message)
-    bot.send_message(message.from_user.id, db.revenue(), reply_markup=keyboard())
+    csv_generator.write_csv(date=datetime.strftime(datetime.now(), "%d.%m.%Y"))
+    report_csv = open('sales.txt', 'rb')
+    bot.send_document(message.chat.id, report_csv, reply_markup=keyboard())
+
+
+@bot.message_handler(commands=["help"])
+def help(message):
+    console_print(message)
+    help_text = '''
+    Чтобы добавить продажу, используй кнопку товара на клавиатуре бота.
+    
+    Команды:
+    /revenue - выручка за сегодня
+    /report - отчет за сегодня
+    
+    Запросы:
+    "Выручка" - выручка за сегодня
+    "Выручка 31.03.2019" - выручка за 31.03.2019
+    "Отчет" - отчет за сегодня
+    "Отчет 31.03.2019" - отчет за 31.03.2019
+    "Отчет полный" - отчет за все дни
+    '''
+    bot.send_message(message.from_user.id, help_text, reply_markup=keyboard())
 
 
 @bot.message_handler(content_types="text")
